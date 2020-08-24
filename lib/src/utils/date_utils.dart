2644bf2b56073a6203b7e53350e42cf26956d90e
Anh Pham <anhphamduy@outlook.com>
2020-08-25 01:31:51 +1000

    Some tests for dateutils

---
+++ lib/src/utils/date_utils.dart
@@ -0,0 +1,221 @@
+enum StartingDayOfWeek {
+  monday,
+  tuesday,
+  wednesday,
+  thursday,
+  friday,
+  saturday,
+  sunday
+}
+
+const numOfDaysInWeek = 7;
+
+class DateUtils {
+  /// Get all the dates in a month
+  static Iterable<DateTime> getDaysInMonth(DateTime month) {
+    final first = firstDayOfMonth(month);
+    final last = lastDayOfMonth(month);
+
+    return getDatesInRange(first, last);
+  }
+
+  /// Get the first date of the month
+  static DateTime firstDayOfMonth(DateTime month) {
+    return DateTime.utc(month.year, month.month, 1, 12);
+  }
+
+  /// Get the last date of the month
+  static DateTime lastDayOfMonth(DateTime month) {
+    final date = DateTime.utc(month.year, month.month + 1, 1);
+    return date.subtract(const Duration(days: 1));
+  }
+
+  /// Check if the two date objects are the same day
+  ///
+  /// Return [true] if two dates are the same, else [false]
+  static bool isSameDay(DateTime dayA, DateTime dayB) {
+    return dayA.year == dayB.year &&
+        dayA.month == dayB.month &&
+        dayA.day == dayB.day;
+  }
+
+  /// Get the number of day before a weekday
+  ///
+  /// Tuesday has one day before it: Monday
+  /// Wednesday has two days before it: Monday and Tuesday
+  static int getNumOfWeekDaysBefore(DateTime firstDay) {
+    return firstDay.weekday - getWeekdayNumber(StartingDayOfWeek.monday);
+  }
+
+  /// Get a weekday number from [StartingDayOfWeek] object
+  ///
+  /// This is equivalent to [DateTime.now().weekday]
+  static int getWeekdayNumber(StartingDayOfWeek weekday) {
+    return StartingDayOfWeek.values.indexOf(weekday) + 1;
+  }
+
+  static Iterable<DateTime> getDatesInRange(
+      DateTime firstDate, DateTime lastDate) sync* {
+    if (isSameDay(firstDate, lastDate)) {
+      yield _normalizedDate(firstDate);
+    }
+    var temp = firstDate;
+
+    while (temp.isBefore(lastDate)) {
+      yield _normalizedDate(temp);
+      temp = temp.add(const Duration(days: 1));
+    }
+    yield _normalizedDate(lastDate);
+  }
+
+  static int getDaysAfter(DateTime lastDay) {
+    int invertedStartingWeekday =
+        8 - getWeekdayNumber(StartingDayOfWeek.monday);
+
+    int daysAfter = 7 - ((lastDay.weekday + invertedStartingWeekday) % 7) + 1;
+    if (daysAfter == 8) {
+      daysAfter = 1;
+    }
+
+    return daysAfter;
+  }
+
+  static bool isDatesConsecutive(dates) {
+    if (dates.length == 1) {
+      return false;
+    }
+
+    for (int i = 0; i < dates.length - 1; i++) {
+      final dateA = dates[i];
+      final dateB = dates[i + 1];
+      // check the difference between two dates
+      if ((dateA.day - dateB.day).abs() != 1) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  static bool isBetweenTwoDates(date, startDate, endDate) {
+    if (isSameDay(date, startDate) || isSameDay(date, endDate)) return true;
+    if (date.isAfter(startDate) && date.isBefore(endDate)) return true;
+    return false;
+  }
+
+  static bool isWeekend(DateTime date) {
+    const saturday = 6;
+    const sunday = 7;
+    return date.weekday == saturday || date.weekday == sunday;
+  }
+
+  static DateTime firstDayOfWeek(DateTime day) {
+    day = _normalizedDate(day);
+
+    final decreaseNum = getNumOfWeekDaysBefore(day);
+    return day.subtract(Duration(days: decreaseNum));
+  }
+
+  static DateTime lastDayOfWeek(DateTime day) {
+    day = _normalizedDate(day);
+
+    final increaseNum = getNumOfWeekDaysBefore(day);
+    return day.add(Duration(days: 7 - increaseNum)).subtract(Duration(days: 1));
+  }
+
+  static bool isInRange(DateTime date, List<DateTime> dates) {
+    for (int i = 0; i < dates.length; i++) {
+      if (isSameDay(date, dates[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /// check if the date is in the current billing period
+  /// this is assumed that the start and end of the billing period would be
+  /// the start and end of the month respectively
+  static bool isCurrentBillingPeriod(DateTime date) {
+    final now = DateTime.now();
+
+    final lastDayOfLastBillingPeriod = DateTime(now.year, now.month, 1 - 1);
+    final startOfTheMonth = DateTime(date.year, date.month, 1);
+
+    if (startOfTheMonth.isBefore(lastDayOfLastBillingPeriod) ||
+        isSameDay(startOfTheMonth, lastDayOfLastBillingPeriod)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  /// Convert the first date to the begining of that date
+  /// and last date to the midnight of that date
+  ///
+  /// Ex: first date is 12/12/2019 will be 12/12/2019.000000
+  /// last date is 13/12/2019 will be 13/12/2019.9999999
+  static List<DateTime> getFirstAndLastTimeOfDateRange(
+      DateTime firstDate, DateTime lastDate) {
+    firstDate = DateTime(firstDate.year, firstDate.month, firstDate.day);
+    lastDate = DateTime.fromMillisecondsSinceEpoch(
+        (DateTime(lastDate.year, lastDate.month, lastDate.day + 1)
+                .millisecondsSinceEpoch) -
+            1);
+
+    return [firstDate, lastDate];
+  }
+
+  /// Get the display format of an hour
+  ///
+  /// Ex: 8.00 will become 08:00
+  static String getDisplayHours(double hours) {
+    String displayHours = hours.toInt().toString();
+    String displayMinutes = ((hours * 60) % 60).toInt().toString();
+
+    if (displayHours.length == 1) {
+      displayHours = '0' + displayHours;
+    }
+    if (displayMinutes.length == 1) {
+      displayMinutes = '0' + displayMinutes;
+    }
+
+    return "$displayHours:$displayMinutes";
+  }
+
+  /// Get the display format of a date from a datetime object
+  ///
+  /// Ex: DateTime(2014, 12, 12) => 12/12/2014
+  /// Datetime of today will be "Today"
+  static String getDisplayDate(DateTime date) {
+    if (date == null) {
+      return "";
+    }
+    String displayedDate = "";
+
+    final DateTime now = DateTime.now();
+
+    if (DateUtils.isSameDay(date, now)) {
+      // if the date is today then display "Today"
+      displayedDate = "Today";
+    } else {
+      String displayMonth = date.month.toString().length == 1
+          ? "0" + date.month.toString()
+          : date.month.toString();
+      String displayDay = date.day.toString().length == 1
+          ? "0" + date.day.toString()
+          : date.day.toString();
+      displayedDate = '$displayDay/$displayMonth/${date.year}';
+    }
+    return displayedDate;
+  }
+
+  static List<DateTime> daysInWeek(DateTime week) {
+    final first = DateUtils.firstDayOfWeek(week);
+    final last = DateUtils.lastDayOfWeek(week);
+    return getDatesInRange(first, last).toList();
+  }
+
+  static DateTime _normalizedDate(DateTime value) {
+    return DateTime.utc(value.year, value.month, value.day);
+  }
+}
---
+++ test/utils/date_utils_test.dart
@@ -0,0 +1,256 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lodlaw_flutter/src/utils/date_utils.dart';
+
+void main() {
+  group('getDaysInMonth tests', () {
+    test('return all the dates for August 2020', () {
+      var date = DateTime(2020, 08, 14, 0, 0, 0);
+      var daysInMonth = DateUtils.getDaysInMonth(date).toList();
+
+      const numOfDaysInAugust = 31;
+      expect(daysInMonth.length, numOfDaysInAugust);
+
+      for (int i = 0; i < numOfDaysInAugust; i++) {
+        expect(daysInMonth[i].year, date.year);
+        expect(daysInMonth[i].month, date.month);
+        expect(daysInMonth[i].day, i + 1);
+      }
+
+      // again but the input is the very last second of the day
+      date = DateTime(2020, 08, 14, 23, 59, 59);
+      daysInMonth = DateUtils.getDaysInMonth(date).toList();
+
+      expect(daysInMonth.length, numOfDaysInAugust);
+
+      for (int i = 0; i < numOfDaysInAugust; i++) {
+        expect(daysInMonth[i].year, date.year);
+        expect(daysInMonth[i].month, date.month);
+        expect(daysInMonth[i].day, i + 1);
+      }
+    });
+
+    test('return all the dates in Feburary 2020 (leap year)', () {
+      final date = DateTime(2020, 02, 10);
+      final daysInMonth = DateUtils.getDaysInMonth(date).toList();
+
+      const numOfDaysInFebruary = 29;
+      expect(daysInMonth.length, numOfDaysInFebruary);
+
+      for (int i = 0; i < numOfDaysInFebruary; i++) {
+        expect(daysInMonth[i].year, date.year);
+        expect(daysInMonth[i].month, date.month);
+        expect(daysInMonth[i].day, i + 1);
+      }
+    });
+
+    test('return all the dates in Feburary 2019 (non-leap year)', () {
+      final date = DateTime(2019, 02, 10);
+      final daysInMonth = DateUtils.getDaysInMonth(date).toList();
+
+      const numOfDaysInFebruary = 28;
+      expect(daysInMonth.length, numOfDaysInFebruary);
+
+      for (int i = 0; i < numOfDaysInFebruary; i++) {
+        expect(daysInMonth[i].year, date.year);
+        expect(daysInMonth[i].month, date.month);
+        expect(daysInMonth[i].day, i + 1);
+      }
+    });
+  });
+
+  group('firstDayOfMonth tests', () {
+    test('return an utc date day of month in August 2020', () {
+      final date = DateTime(2020, 8, 12);
+      final firstDayOfMonth = DateUtils.firstDayOfMonth(date);
+
+      final expected = DateTime.utc(2020, 8, 1);
+      expect(firstDayOfMonth.day, expected.day);
+      expect(firstDayOfMonth.month, expected.month);
+      expect(firstDayOfMonth.year, expected.year);
+    });
+
+    test('return an utc date day of month in Feburary 2020', () {
+      final date = DateTime(2020, 2, 29);
+      final firstDayOfMonth = DateUtils.firstDayOfMonth(date);
+
+      final expected = DateTime.utc(2020, 2, 1);
+      expect(firstDayOfMonth.day, expected.day);
+      expect(firstDayOfMonth.month, expected.month);
+      expect(firstDayOfMonth.year, expected.year);
+    });
+  });
+
+  group('lastDayOfMonth tests', () {
+    test('return an utc date day of month in December 2020', () {
+      final date = DateTime(2020, 12, 4);
+      final firstDayOfMonth = DateUtils.lastDayOfMonth(date);
+
+      final expected = DateTime.utc(2020, 12, 31);
+      expect(firstDayOfMonth.day, expected.day);
+      expect(firstDayOfMonth.month, expected.month);
+      expect(firstDayOfMonth.year, expected.year);
+    });
+
+    test('return an utc date in Janurary 2020', () {
+      final date = DateTime(2020, 1, 1);
+      final firstDayOfMonth = DateUtils.lastDayOfMonth(date);
+
+      final expected = DateTime.utc(2020, 1, 31);
+      expect(firstDayOfMonth.day, expected.day);
+      expect(firstDayOfMonth.month, expected.month);
+      expect(firstDayOfMonth.year, expected.year);
+    });
+  });
+
+  group('isSameDay tests', () {
+    test('29/02/2020 should be the same day as 29/09/2020', () {
+      final firstDate = DateTime(2020, 2, 29);
+      final secondDate = DateTime(2020, 2, 29, 12, 12, 12);
+      expect(DateUtils.isSameDay(firstDate, secondDate), true);
+    });
+
+    test('29/02/2020 should different from 01/01/2020', () {
+      final firstDate = DateTime(2020, 2, 29);
+      final secondDate = DateTime(2020, 1, 1, 12, 12, 12);
+      expect(DateUtils.isSameDay(firstDate, secondDate), false);
+    });
+  });
+
+  test('getWeekdayNumber tests', () {
+    final monday = DateUtils.getWeekdayNumber(StartingDayOfWeek.monday);
+    expect(monday, 1);
+
+    final tuesday = DateUtils.getWeekdayNumber(StartingDayOfWeek.tuesday);
+    expect(tuesday, 2);
+
+    final wednesday = DateUtils.getWeekdayNumber(StartingDayOfWeek.wednesday);
+    expect(wednesday, 3);
+
+    final thursday = DateUtils.getWeekdayNumber(StartingDayOfWeek.thursday);
+    expect(thursday, 4);
+
+    final friday = DateUtils.getWeekdayNumber(StartingDayOfWeek.friday);
+    expect(friday, 5);
+
+    final saturday = DateUtils.getWeekdayNumber(StartingDayOfWeek.saturday);
+    expect(saturday, 6);
+
+    final sunday = DateUtils.getWeekdayNumber(StartingDayOfWeek.sunday);
+    expect(sunday, 7);
+  });
+
+  group('getDatesInRange tests', () {
+    test('get all the dates from 12/08/2020 to 16/08/2020', () {
+      final firstDate = DateTime(2020, 8, 12);
+      final lastDate = DateTime(2020, 8, 16);
+      final dates = DateUtils.getDatesInRange(firstDate, lastDate).toList();
+
+      expect(dates.length, lastDate.difference(firstDate).inDays + 1);
+
+      for (int i = 0; i < dates.length; i++) {
+        final date = dates[i];
+
+        final expectedDate =
+            DateTime.utc(firstDate.year, firstDate.month, firstDate.day + i);
+        expect(date.day, expectedDate.day);
+        expect(date.month, expectedDate.month);
+        expect(date.year, expectedDate.year);
+      }
+    });
+
+    test('get all the dates from 29/08/2020 to 02/09/2020', () {
+      final firstDate = DateTime(2020, 8, 29);
+      final lastDate = DateTime(2020, 9, 2);
+      final dates = DateUtils.getDatesInRange(firstDate, lastDate).toList();
+
+      expect(dates.length, lastDate.difference(firstDate).inDays + 1);
+
+      for (int i = 0; i < dates.length; i++) {
+        final date = dates[i];
+
+        final expectedDate =
+            DateTime.utc(firstDate.year, firstDate.month, firstDate.day + i);
+        expect(date.day, expectedDate.day);
+        expect(date.month, expectedDate.month);
+        expect(date.year, expectedDate.year);
+      }
+    });
+
+    test('get all the dates from 27/02/2020 to 02/03/2020 (leap year)', () {
+      final firstDate = DateTime(2020, 2, 27);
+      final lastDate = DateTime(2020, 3, 2);
+      final dates = DateUtils.getDatesInRange(firstDate, lastDate).toList();
+
+      expect(dates.length, lastDate.difference(firstDate).inDays + 1);
+
+      for (int i = 0; i < dates.length; i++) {
+        final date = dates[i];
+
+        final expectedDate =
+            DateTime.utc(firstDate.year, firstDate.month, firstDate.day + i);
+        expect(date.day, expectedDate.day);
+        expect(date.month, expectedDate.month);
+        expect(date.year, expectedDate.year);
+      }
+    });
+
+    test('get all the dates from 27/02/2019 to 02/03/2019 (non-leap year)', () {
+      final firstDate = DateTime(2019, 2, 27);
+      final lastDate = DateTime(2019, 3, 2);
+      final dates = DateUtils.getDatesInRange(firstDate, lastDate).toList();
+
+      expect(dates.length, lastDate.difference(firstDate).inDays + 1);
+
+      for (int i = 0; i < dates.length; i++) {
+        final date = dates[i];
+
+        final expectedDate =
+            DateTime.utc(firstDate.year, firstDate.month, firstDate.day + i);
+        expect(date.day, expectedDate.day);
+        expect(date.month, expectedDate.month);
+        expect(date.year, expectedDate.year);
+      }
+    });
+
+    test('get all the dates from 31/12/2019 to 03/01/2020', () {
+      final firstDate = DateTime(2019, 12, 31);
+      final lastDate = DateTime(2020, 1, 3);
+      final dates = DateUtils.getDatesInRange(firstDate, lastDate).toList();
+
+      expect(dates.length, lastDate.difference(firstDate).inDays + 1);
+
+      for (int i = 0; i < dates.length; i++) {
+        final date = dates[i];
+
+        final expectedDate =
+            DateTime.utc(firstDate.year, firstDate.month, firstDate.day + i);
+        expect(date.day, expectedDate.day);
+        expect(date.month, expectedDate.month);
+        expect(date.year, expectedDate.year);
+      }
+    });
+  });
+
+  test('getNumOfWeekDaysBefore tests', () {
+    final monday = DateTime(2020, 8, 17);
+    expect(DateUtils.getNumOfWeekDaysBefore(monday), 0);
+
+    final tuesday = DateTime(2020, 8, 18);
+    expect(DateUtils.getNumOfWeekDaysBefore(tuesday), 1);
+
+    final wednesday = DateTime(2020, 8, 19);
+    expect(DateUtils.getNumOfWeekDaysBefore(wednesday), 2);
+
+    final thursday = DateTime(2020, 8, 20);
+    expect(DateUtils.getNumOfWeekDaysBefore(thursday), 3);
+
+    final friday = DateTime(2020, 8, 21);
+    expect(DateUtils.getNumOfWeekDaysBefore(friday), 4);
+
+    final saturday = DateTime(2020, 8, 22);
+    expect(DateUtils.getNumOfWeekDaysBefore(saturday), 5);
+
+    final sunday = DateTime(2020, 8, 23);
+    expect(DateUtils.getNumOfWeekDaysBefore(sunday), 6);
+  });
+}
