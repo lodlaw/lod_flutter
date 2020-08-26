d2712acb628cfd51d78a2b554b907c4fbb3ef689
Anh Pham <anhphamduy@outlook.com>
2020-08-26 12:13:57 +1000

    Change scrolling spinner to using cupertino spinner

--- lib/src/widgets/input_field/scrolling_spinner.dart
+++ lib/src/widgets/input_field/scrolling_spinner.dart
@@ -1,78 +1,55 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:lodlaw_flutter/src/physics/infinite_scroll.dart';
 import 'package:lodlaw_flutter/src/widgets/input_field/input_field.dart';
 
-typedef SelectedIndexCallback = void Function(int);
+const _squeeze = 0.9;
+const _diameterRatio = 1.9;
+const _numOfRenderedElements = 3;
+const double _width = 30;
 
 class ScrollingSpinner extends StatefulWidget {
-  final DateTime time;
-  final TextStyle highlightedTextStyle;
-  final TextStyle normalTextStyle;
-  final double itemHeight;
-  final double spacing;
-  final void Function(String item) onChange;
+  /// The width of the spinner
+  final double width;
+
+  /// Callback when an item is changed
+  final Function(String item) onChange;
+
+  /// A list of items to be rendered
   final List<String> items;
-  final String defaultValue;
+
+  /// The current value of the spinner
+  final String value;
+
+  /// Hint text for the spinner
   final String hintText;
 
-  ScrollingSpinner(
-      {Key key,
-      this.time,
-      this.highlightedTextStyle,
-      this.normalTextStyle,
-      this.itemHeight,
-      this.spacing = 30,
-      this.items,
-      this.onChange,
-      this.defaultValue,
-      this.hintText})
-      : super(key: key);
+  /// An uncontrolled cupertino style loop spinner
+  ScrollingSpinner({
+    Key key,
+    this.width = _width,
+    this.items,
+    this.onChange,
+    this.value,
+    this.hintText = "",
+  })  : assert(hintText != null),
+        super(key: key);
 
   @override
   _ScrollingSpinnerState createState() => _ScrollingSpinnerState();
 }
 
 class _ScrollingSpinnerState extends State<ScrollingSpinner> {
-  ScrollController _scrollController = ScrollController();
-  int _currentSelectedIndex = 3;
-  bool isScrolling = false;
-
-  double defaultItemHeight = 60;
-
-  /// getter
-  TextStyle _getHighlightedTextStyle() {
-    return Theme.of(context).textTheme.subtitle1;
-  }
-
-  TextStyle _getNormalTextStyle() {
-    return Theme.of(context).textTheme.subtitle1;
-  }
-
-  double _getItemHeight() {
-    return InputField.height;
-  }
-
-  bool isLoop(int value) {
-    return value > widget.items.length - 3;
-  }
-
-  String getSelectedItem() {
-    return widget.items[_currentSelectedIndex % widget.items.length];
-  }
+  FixedExtentScrollController _scrollController = FixedExtentScrollController();
 
   @override
   void initState() {
     super.initState();
 
-    if (widget.defaultValue != null) {
+    // find the position of the value and go to that position in the first render
+    if (widget.value != null) {
       for (int i = 0; i < widget.items.length; i++) {
-        if (widget.items[i] == widget.defaultValue) {
-          if (i == 0) {
-            i = widget.items.length;
-          }
-          _currentSelectedIndex = i;
-          _scrollController =
-              ScrollController(initialScrollOffset: (i - 1) * _getItemHeight());
+        if (widget.items[i] == widget.value) {
+          _scrollController = FixedExtentScrollController(initialItem: i);
           break;
         }
       }
@@ -80,126 +57,117 @@ class _ScrollingSpinnerState extends State<ScrollingSpinner> {
   }
 
   @override
+  void didUpdateWidget(oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    // if list of items has changed, find the new position for the updated value
+    // and go to that position
+    if (oldWidget.items.length != widget.items.length) {
+      int updatedPosition;
+
+      // find the new position of the item
+      for (int i = 0; i < widget.items.length; i++) {
+        if (widget.items[i] == widget.value) {
+          updatedPosition = i;
+        }
+      }
+
+      if (updatedPosition != null) {
+        // this must be rendered in the next frame because jumpToItem will be
+        // rendered during setState
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          _scrollController.jumpToItem(updatedPosition);
+        });
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    List<Widget> contents = [
-      SizedBox(
-        width: widget.spacing,
-        height: _getItemHeight() * 3,
-        child: spinner(
-          _scrollController,
-          widget.items.length,
-          _currentSelectedIndex,
-          1,
-          (index) {
-            _currentSelectedIndex = index;
-            isScrolling = true;
-          },
-          () => isScrolling = false,
-        ),
+    final spinner = SizedBox(
+      width: widget.width,
+      height: _getItemHeight() * _numOfRenderedElements,
+      child: CupertinoPicker(
+        scrollController: _scrollController,
+        itemExtent: _getItemHeight(),
+        onSelectedItemChanged: (index) {
+          widget.onChange(widget.items[index]);
+        },
+        squeeze: _squeeze,
+        diameterRatio: _diameterRatio,
+        looping: true,
+        children: widget.items
+            .map((e) => SpinningItem(
+                  content: e,
+                ))
+            .toList(),
       ),
-    ];
+    );
+
     return Stack(
       children: [
-        _buildHintText(),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: contents,
+        HintText(
+          content: widget.hintText,
         ),
+        spinner,
       ],
     );
   }
 
-  Widget _buildHintText() {
+  double _getItemHeight() {
+    return InputField.height;
+  }
+}
+
+@visibleForTesting
+class SpinningItem extends StatelessWidget {
+  /// The text to be displayed
+  final String content;
+
+  /// The spinning item
+  const SpinningItem({Key key, @required this.content})
+      : assert(content != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          content,
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
+      ],
+    );
+  }
+}
+
+@visibleForTesting
+class HintText extends StatelessWidget {
+  /// The text to be displayed
+  final String content;
+
+  /// The hint text to be displayed under the selected item of the
+  /// spinner
+  const HintText({Key key, @required this.content})
+      : assert(content != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Positioned.fill(
       child: Padding(
           padding: EdgeInsets.only(top: 2 * InputField.height),
           child: Text(
-            widget.hintText,
+            content,
             textAlign: TextAlign.center,
-            style: _getNormalTextStyle().apply(fontSizeFactor: 0.9),
+            style: Theme.of(context)
+                .textTheme
+                .subtitle1
+                .apply(fontSizeFactor: 0.9),
           )),
-    );
-  }
-
-  Widget spinner(
-      ScrollController controller,
-      int max,
-      int selectedIndex,
-      int interval,
-      SelectedIndexCallback onUpdateSelectedIndex,
-      VoidCallback onScrollEnd) {
-    /// wrapping the spinner with stack and add container above it when it's scrolling
-    /// this thing is to prevent an error causing by some weird stuff like this
-    /// flutter: Another exception was thrown: 'package:flutter/src/widgets/scrollable.dart': Failed assertion: line 469 pos 12: '_hold == null || _drag == null': is not true.
-    /// maybe later we can find out why this error is happening
-    Widget _spinner = NotificationListener<ScrollNotification>(
-      onNotification: (scrollNotification) {
-        if (scrollNotification is UserScrollNotification) {
-          if (scrollNotification.direction.toString() ==
-              "ScrollDirection.idle") {
-            if (isLoop(max)) {
-              int segment = (selectedIndex / max).floor();
-              if (segment == 0) {
-                onUpdateSelectedIndex(selectedIndex + max);
-                controller.jumpTo(controller.offset + (max * _getItemHeight()));
-              } else if (segment == 2) {
-                onUpdateSelectedIndex(selectedIndex - max);
-                controller.jumpTo(controller.offset - (max * _getItemHeight()));
-              }
-            }
-            setState(() {
-              onScrollEnd();
-              if (widget.onChange != null) {
-                widget.onChange(getSelectedItem());
-              }
-            });
-          }
-        } else if (scrollNotification is ScrollUpdateNotification) {
-          setState(() {
-            onUpdateSelectedIndex(
-                (controller.offset / _getItemHeight()).round() + 1);
-            widget.onChange(getSelectedItem());
-          });
-        }
-        return true;
-      },
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return SizedBox(
-            height: _getItemHeight(),
-            width: 10,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  widget.items[index % widget.items.length],
-                  style: selectedIndex == index
-                      ? _getHighlightedTextStyle()
-                      : _getNormalTextStyle(),
-                ),
-              ],
-            ),
-          );
-        },
-        controller: controller,
-        itemCount: isLoop(max) ? max * 3 : max + 2,
-        physics: InfiniteScrollPhysics(itemHeight: _getItemHeight()),
-        padding: EdgeInsets.zero,
-      ),
-    );
-
-    return Stack(
-      children: <Widget>[
-        Positioned.fill(child: _spinner),
-        isScrolling
-            ? Positioned.fill(
-                child: Container(
-                color: Colors.black.withOpacity(0),
-              ))
-            : Container()
-      ],
     );
   }
 }
